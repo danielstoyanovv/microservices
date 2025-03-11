424474fbdb877bc82ce93d7d2136b2a17bd9b56e
danielstoyanovv <daniel.stoyanovv@gmail.com>
2025-03-11 12:39:59 +0200

    Update App.js - integrate post list component Create PostCreate.js - integrate post create component - add new form Create PostList.js - integrate post list component

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import axios from "axios";
 
 export default () => {
     const [title, setTitle] = useState("")
---
+++ client/src/PostList.js
@@ -0,0 +1,60 @@
+import React, {useEffect, useState} from "react";
+
+export default () => {
+    const [posts, setPosts] = useState([])
+
+    const fetchPosts = async () => {
+        const response = await fetch("http://localhost:4000/posts", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        })
+        const json = response.json()
+        if (response.ok) {
+            json.then(result => {
+                // console.log(result.data)
+                setPosts(result.data)
+                const posts = []
+                Object.keys(result.data).map(postKeys => {
+                    const id = postKeys
+                    Object.values(result.data).map(postValue => {
+                        const title = postValue
+                        const post = {
+                            id: id,
+                            title: title
+                        };
+                        posts.push(post)
+
+                    })
+                })
+                setPosts(posts)
+            })
+        }
+    }
+
+    const renderPosts =  Object.values(posts).map(post => {
+        return (
+            <div
+                className="card"
+                key={post.id}
+            >
+                <div
+                    className="card-body"
+                >
+                    <h3>{post.title}</h3>
+                </div>
+
+            </div>
+        )
+    })
+
+    useEffect(() => {
+        fetchPosts()
+    }, []);
+
+    return <div className="d-flex flex-row flex-wrap justify-content-between">
+        {renderPosts}
+    </div>
+}
