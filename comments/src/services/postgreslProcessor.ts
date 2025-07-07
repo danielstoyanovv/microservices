773147bd6880432adfa24487dcc9f81a0f75ae52
danielstoyanovv <daniel.stoyanovv@gmail.com>
2025-07-07 14:34:39 +0300

    Update create-db.ts - add PostgresProcessor in logic Add databaseProcessor.ts - abstract method createDatabase added - abstract method getConnectionDriver added Add postgreslProcessor.ts - extends abstract class DatabaseProcessor - implement createDatabase method - implement getConnectionDriver method

---
+++ comments/src/services/postgreslProcessor.ts
@@ -0,0 +1,42 @@
+"use strict"
+
+import {DatabaseProcessor} from "./databaseProcessor";
+const { Pool } = require('pg');
+require('dotenv').config();
+import {LoggerService} from "./LoggerService";
+
+const logger = new LoggerService().createLogger()
+
+const DB_NAME = process.env.NODE_ENV === 'test' ? process.env.PG_DB +  "_test" : process.env.PG_DB
+
+export class PostgresProcessor extends DatabaseProcessor {
+    /**
+     * createDatabase
+     * @return {void}
+     */
+    async createDatabase() {
+        const client = await this.getConnectionDriver()
+        client
+            .query(`CREATE DATABASE ` + DB_NAME)
+            .catch((err: any) => logger.error(err))
+    }
+
+    /**
+     * get Connection Driver
+     * @return {object}
+     */
+    async getConnectionDriver() {
+        return new Pool({
+            user: process.env.PG_USER || "postgres",
+            host: process.env.PG_HOST || "127.0.0.1",
+            database: "postgres",
+            password: process.env.PG_PASSWORD || "PASSWORD",
+            port: process.env.PG_PORT || 5432,
+            ssl:
+                process.env.NODE_ENV !== 'production'
+                    ? false
+                    : { rejectUnauthorized: false },
+        });
+    }
+
+}
